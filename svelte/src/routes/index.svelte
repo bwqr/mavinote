95ee7c02726bdfaabb9ff7f846bb1821f42ef687
bwqr <ruzgardeniz.08@hotmail.com>
2022-05-14 12:49:57 +0300

    init svelte project

---
+++ svelte/src/routes/index.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+    import init, { greet } from 'wasm';
+
+    init().then(res => console.log(greet())).catch(e => console.error('error', e));
+</script>
+
+<div>
+    <h1>Hello World!</h1>
+</div>
