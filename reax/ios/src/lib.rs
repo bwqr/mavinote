7cfe0faac4ff1f29dbe7756adca736bd918f9982
bwqr <ruzgardeniz.08@hotmail.com>
2022-05-15 19:21:00 +0300

    refactor reax ios

--- reax/ios/src/lib.rs
+++ reax/ios/src/lib.rs
@@ -1,11 +1,15 @@
-use std::{os::raw::{c_char, c_int, c_uchar}, ffi::{CStr, c_void}, sync::{Mutex, mpsc::Sender}};
+use std::{os::raw::{c_char, c_int, c_uchar}, ffi::{CStr, c_void}, sync::{Mutex, mpsc::Sender}, future::Future};
 
+use base::{Store, Config};
 use once_cell::sync::OnceCell;
+use reqwest::{header::{HeaderMap, HeaderValue}, ClientBuilder, Client};
 use serde::Serialize;
+use tokio::task::JoinHandle;
 
 mod note;
 mod auth;
 
+static ASYNC_RUNTIME: OnceCell<tokio::runtime::Runtime> = OnceCell::new();
 static HANDLER: OnceCell<Mutex<Sender<(i32, bool, Vec<u8>)>>> = OnceCell::new();
 
 pub(crate) fn send<T: Serialize, E: Serialize>(wait_id: i32, res: Result<T, E>) {
@@ -17,6 +21,14 @@ pub(crate) fn send<T: Serialize, E: Serialize>(wait_id: i32, res: Result<T, E>) 
     HANDLER.get().unwrap().lock().unwrap().send((wait_id, res.is_ok(), bytes)).unwrap();
 }
 
+pub fn spawn<F>(future: F) -> JoinHandle<F::Output>
+where
+    F: Future + Send + 'static,
+    F::Output: Send + 'static,
+{
+    ASYNC_RUNTIME.get().unwrap().spawn(future)
+}
+
 #[no_mangle]
 pub extern fn reax_init(api_url: *const c_char, storage_dir: *const c_char) {
     let api_url = unsafe { CStr::from_ptr(api_url).to_str().unwrap().to_string() };
@@ -25,9 +37,34 @@ pub extern fn reax_init(api_url: *const c_char, storage_dir: *const c_char) {
     std::env::set_var("RUST_LOG", "debug");
     env_logger::init();
 
-    runtime::init(base::Config { api_url, storage_dir });
+    ASYNC_RUNTIME
+        .set(
+            tokio::runtime::Builder::new_multi_thread()
+                .enable_all()
+                .build()
+                .expect("failed to initialize tokio runtime"),
+        )
+        .expect("failed to set tokio runtime");
 
-    log::info!("reax runtime is initialized");
+    runtime::init();
+
+    let mut headers = HeaderMap::new();
+    headers.insert("Content-Type", HeaderValue::from_static("application/json"));
+    let client = ClientBuilder::new()
+        .default_headers(headers)
+        .build()
+        .unwrap();
+
+    runtime::put::<Client>(client);
+
+    runtime::put::<Store>(Store);
+
+    runtime::put::<Config>(Config {
+        api_url,
+        storage_dir,
+    });
+
+    ::log::info!("reax runtime is initialized");
 }
 
 #[no_mangle]
--- reax/ios/src/note.rs
+++ reax/ios/src/note.rs
@@ -1,60 +1,102 @@
-use std::{os::raw::{c_int, c_char}, ffi::CStr};
+use std::{
+    ffi::CStr,
+    os::raw::{c_char, c_int},
+};
+
+use base::{Config, Store};
+use reqwest::Client;
 
 use crate::send;
 
 #[no_mangle]
-pub extern fn reax_note_folders(wait_id: c_int) {
-    runtime::spawn(async move {
-        let res = note::folders(runtime::store(), runtime::client(), runtime::config()).await;
+pub extern "C" fn reax_note_folders(wait_id: c_int) {
+    crate::spawn(async move {
+        let res = note::folders(
+            runtime::get::<Store>().unwrap(),
+            runtime::get::<Client>().unwrap(),
+            runtime::get::<Config>().unwrap(),
+        )
+        .await;
 
         send(wait_id, res);
     });
 }
 
 #[no_mangle]
-pub extern fn reax_note_create_folder(wait_id: c_int, name: * const c_char) {
+pub extern "C" fn reax_note_create_folder(wait_id: c_int, name: *const c_char) {
     let name = unsafe { CStr::from_ptr(name).to_str().unwrap().to_string() };
 
-    runtime::spawn(async move {
-        let res = note::create_folder(runtime::store(), runtime::client(), runtime::config(), name).await;
+    crate::spawn(async move {
+        let res = note::create_folder(
+            runtime::get::<Store>().unwrap(),
+            runtime::get::<Client>().unwrap(),
+            runtime::get::<Config>().unwrap(),
+            name,
+        )
+        .await;
 
         send(wait_id, res);
     });
 }
 
 #[no_mangle]
-pub extern fn reax_note_note_summaries(wait_id: c_int, folder_id: c_int) {
-    runtime::spawn(async move {
-        let res = note::note_summaries(runtime::store(), runtime::client(), runtime::config(), folder_id).await;
+pub extern "C" fn reax_note_note_summaries(wait_id: c_int, folder_id: c_int) {
+    crate::spawn(async move {
+        let res = note::note_summaries(
+            runtime::get::<Store>().unwrap(),
+            runtime::get::<Client>().unwrap(),
+            runtime::get::<Config>().unwrap(),
+            folder_id,
+        )
+        .await;
 
         send(wait_id, res);
     });
 }
 
 #[no_mangle]
-pub extern fn reax_note_note(wait_id: c_int, note_id: c_int) {
-    runtime::spawn(async move {
-        let res = note::note(runtime::store(), runtime::client(), runtime::config(), note_id).await;
+pub extern "C" fn reax_note_note(wait_id: c_int, note_id: c_int) {
+    crate::spawn(async move {
+        let res = note::note(
+            runtime::get::<Store>().unwrap(),
+            runtime::get::<Client>().unwrap(),
+            runtime::get::<Config>().unwrap(),
+            note_id,
+        )
+        .await;
 
         send(wait_id, res);
     });
 }
 
 #[no_mangle]
-pub extern fn reax_note_create_note(wait_id: c_int, folder_id: c_int) {
-    runtime::spawn(async move {
-        let res = note::create_note(runtime::store(), runtime::client(), runtime::config(), folder_id).await;
+pub extern "C" fn reax_note_create_note(wait_id: c_int, folder_id: c_int) {
+    crate::spawn(async move {
+        let res = note::create_note(
+            runtime::get::<Store>().unwrap(),
+            runtime::get::<Client>().unwrap(),
+            runtime::get::<Config>().unwrap(),
+            folder_id,
+        )
+        .await;
 
         send(wait_id, res);
     });
 }
 
 #[no_mangle]
-pub extern fn reax_note_update_note(wait_id: c_int, note_id: c_int, text: * const c_char) {
+pub extern "C" fn reax_note_update_note(wait_id: c_int, note_id: c_int, text: *const c_char) {
     let text = unsafe { CStr::from_ptr(text).to_str().unwrap().to_string() };
 
-    runtime::spawn(async move {
-        let res = note::update_note(runtime::store(), runtime::client(), runtime::config(), note_id, text).await;
+    crate::spawn(async move {
+        let res = note::update_note(
+            runtime::get::<Store>().unwrap(),
+            runtime::get::<Client>().unwrap(),
+            runtime::get::<Config>().unwrap(),
+            note_id,
+            text,
+        )
+        .await;
 
         send(wait_id, res);
     });
