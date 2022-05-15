e2179a6070db55d0ccde196727298bd720021078
bwqr <ruzgardeniz.08@hotmail.com>
2022-05-15 19:11:53 +0300

    remove sqlx from store temporarily

--- reax/base/src/store.rs
+++ reax/base/src/store.rs
@@ -1,51 +1,12 @@
-use sqlx::{Row, Pool, Sqlite};
-
 #[derive(Debug)]
-pub struct Store {
-    pool: &'static Pool<Sqlite>
-}
+pub struct Store;
 
 impl Store {
-    pub fn new(pool: &'static Pool<Sqlite>) -> Self {
-        Self { pool }
+    pub async fn get(&self, key: &str) -> Result<Option<String>, crate::Error> {
+        Ok(Some("Hello".to_string()))
     }
 
-    pub async fn get(&self, key: &str) -> Result<Option<String>, sqlx::Error> {
-        let mut conn = self.pool.acquire().await?;
-
-        let row = sqlx::query("select value from store where key = ?")
-            .bind(key)
-            .fetch_optional(&mut conn)
-            .await?;
-
-        if let Some(row) = row {
-            row.try_get("value")
-        } else {
-            Ok(None)
-        }
-    }
-
-    pub async fn put(&self, key: &str, value: &str) -> Result<(), sqlx::Error> {
-        let mut conn = self.pool.acquire().await?;
-
-        let res = sqlx::query("insert into store values (?, ?)")
-            .bind(key)
-            .bind(value)
-            .execute(&mut conn)
-            .await
-            .map(|_| ());
-
-        if let Err(sqlx::Error::Database(e)) = &res {
-            if let Some("2067") = e.code().as_deref() {
-                return sqlx::query("update store set value = ? where key = ?")
-                    .bind(value)
-                    .bind(key)
-                    .execute(&mut conn)
-                    .await
-                    .map(|_| ())
-            }
-        }
-
-        res
+    pub async fn put(&self, key: &str, value: &str) -> Result<(), crate::Error> {
+        Ok(())
     }
 }
